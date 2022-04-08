030977962cabd58c20e62f5e3f5d4ef90d906545
EmelineFavreau <emeline.favreau.15@ucl.ac.uk>
2022-04-08 16:56:31 +0100

    tidying assembly scaffolding and qc

--- 02-genome_assembly/scaffolding-assembly.sh
+++ 02-genome_assembly/scaffolding-assembly.sh
@@ -1,7 +1,15 @@
+# create a gff for maker input 
+module load  augustus/3.2.3
+module load  maker/2.31.9
+maker -CTL
+maker -cpu 4
+find .  -type f -name '*gff' ! -path '*theVoid*' | parallel cat {} > reference.gff
+tar -czvf reference.maker.output.tgz reference.maker.output 
+
+# mapping rna reads 
 module load bwa/0.7.17
 module load samtools/1.9
  
-#######
 # map pairs of reads with bwa  (STAR had no pairs on different scaffolds)
 mkdir -p tmp/genome
 cp input/reference.fa tmp/genome
@@ -15,12 +23,11 @@ sh commands.sh > tmp/map/log 2> tmp/map/err
 # they are sorted by read name; keeping this when merging (Agouti takes 1 file). Removing multipmapping reads: qual minimum 50 and those with SA:Z
 samtools merge -n - tmp/map/SRR1325085.bam tmp/map/SRR1325086.bam tmp/map/SRR1325087.bam tmp/map/SRR1325088.bam | samtools view -b -q 50 - > tmp/map_merged.bam
 
-# removing all multiple-mapping reads (needed by Agouti), and low quality mapping: 
-#  UNFORTUNATELY, SOME MULTIPLE MAPPED REMAIN... UNSURE HOW TO REMOVE THEM ! (SA:Z indicates something but I'm not sure its this!)
-
 mv tmp/map_merged.bam results/FourRnaseq_bwa_pheidoleE.bam
-#####
+
 # Then run agouti. 
-#module load intelpython/2.7.12
-#mkdir tmp/agouti
-#python ../../soft/AGOUTI/agouti.py scaffold -assembly input/reference.fa -bam tmp/map_merged_q100.bam -outdir tmp/agouti
+module load intelpython/2.7.12
+mkdir tmp/agouti
+python ../../soft/AGOUTI/agouti.py scaffold -assembly input/reference.fa -bam tmp/map_merged_q100.bam -outdir tmp/agouti
+
+
